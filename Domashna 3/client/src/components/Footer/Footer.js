ee771249bb980f0c6b2351f8948b7dcddc22e667
ProektDians <proektdians1@gmail.com>
2021-12-15 12:28:27 +0100

    Kreirajne na lenta za navigacija i footer za Web App

---
+++ Domashna 3/client/src/components/Footer/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './Footer.css'
+export default function Footer(){
+return (
+        <div className="footer">
+
+
+            <p>191089 Славчо Прентоски </p>
+            <p>191113 Марија Пајкоска </p>
+            <p>191002 Сара Ставреска </p>
+            <p>191015 Дона Трајкоска </p>
+            <p>191139 Ана-Марија Пиперкоска </p>
+
+
+        </div>
+
+
+)
+
+
+}
